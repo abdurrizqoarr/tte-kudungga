6ef16274335149063164f783e8a2cb927a8c2215
abdurrizqoarra <abdurrizqoaa@gmail.com>
2025-10-06 09:04:35 +0800

    tes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/sign/resume-ralan', [SignTteController::class, 'resumeRalan']);
 Route::post('/sign/resume-ranap', [SignTteController::class, 'resumeRanap']);
 Route::post('/sign/dokumen-with-qr', [SignTteController::class, 'signDokumenWithQr']);
+Route::post('/sign/dokumen-no-qr', [SignTteController::class, 'signDokumenNoQr']);
 
 Route::get('/sign/download/{id_dokumen}', [SignTteController::class, 'downloadFile']);
 Route::post('/sign/verify', [SignTteController::class, 'verifySign']);
