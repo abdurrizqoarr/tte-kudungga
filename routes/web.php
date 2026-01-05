90e4f351ff5be9eac06673b3a3125eeac1840fcf
abdurrizqoarr <abdurrizqoaa@gmail.com>
2026-01-05 10:33:12 +0800

    Fix route for verifikasi-dokumen-ranap

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\InformController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/verifikasi-dokumen-ralan/{idDokumen}', [InformController::class, 'informPageRalan']);
-Route::get('/verifikasi-dokumen-ramap/{idDokumen}', [InformController::class, 'informPageRanap']);
+Route::get('/verifikasi-dokumen-ranap/{idDokumen}', [InformController::class, 'informPageRanap']);
