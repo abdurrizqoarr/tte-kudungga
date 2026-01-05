5008989e8777dbc06f42f4767887fa28bd1c8db8
abdurrizqoarr <abdurrizqoaa@gmail.com>
2026-01-05 11:02:42 +0800

    Replace RiwayatResumeRamap with RiwayatResumeRanap

--- app/Http/Controllers/InformController.php
+++ app/Http/Controllers/InformController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\RiwayatResumeRalan;
-use App\Models\RiwayatResumeRamap;
+use App\Models\RiwayatResumeRanap;
 use Illuminate\Http\Request;
 
 class InformController extends Controller
@@ -17,7 +17,7 @@ class InformController extends Controller
 
     public function informPageRanap($idDokumen)
     {
-        $dokumen = RiwayatResumeRamap::where('id', $idDokumen)->first();
+        $dokumen = RiwayatResumeRanap::where('id', $idDokumen)->first();
 
         return view('inform', ["data" => $dokumen]);
     }
